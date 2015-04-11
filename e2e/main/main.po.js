595688e21c74545bd2a25cfad01504e0fb121c9d
Shigeru Kondou <concigel@gmail.com>
2015-04-12 00:03:08 +0900

    add test `add`

--- e2e/main/main.po.js
+++ e2e/main/main.po.js
@@ -7,8 +7,14 @@
 
 var MainPage = function() {
   this.heroEl = element(by.css('.hero-unit'));
-  this.h1El = this.heroEl.element(by.css('h1'));
+  this.h1El  = this.heroEl.element(by.css('h1'));
   this.imgEl = this.heroEl.element(by.css('img'));
+
+  this.nameEl = element(by.id('nameInput'));
+  this.infoEl = element(by.id('infoInput'));
+  this.createEl = element(by.id('createOrUpdate'));
+
+  this.features = element.all(by.repeater('thing in awesomeThings'));
 };
 
 module.exports = new MainPage();
--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -5,20 +5,41 @@ var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 /* jshint -W079 */
 var expect = chai.expect;
+var HttpBackend = require('httpbackend');
+var backend = null;
 
 describe('Main View', function() {
-  var page;
+  var page = {};
 
   beforeEach(function() {
+    backend = new HttpBackend(browser);
+    backend.whenGET(/\//).passThrough();
+
     browser.get('/');
-    page = require('./main.po');
+    page.main = require('./main.po');
+  });
+
+  afterEach(function() {
+    backend.clear();
   });
 
   it('Jumbotron に正しいデータが含まれる', function() {
-    expect(page.h1El.getText()).to.eventually.equal('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).to.eventually.match(/assets\/images\/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).to.eventually.equal('I\'m Yeoman');
+    backend.whenGET(/.*/).passThrough();
+
+    expect(page.main.h1El.getText()).to.eventually.equal('\'Allo, \'Allo!');
+    expect(page.main.imgEl.getAttribute('src')).to.eventually.match(/assets\/images\/yeoman.png$/);
+    expect(page.main.imgEl.getAttribute('alt')).to.eventually.equal('I\'m Yeoman');
   });
 
+  it('追加', function() {
+    backend.whenGET(/.*/).passThrough();
+    backend.whenPOST(/.*/).passThrough();
+    
+    page.main.nameEl.sendKeys('aaa');
+    page.main.infoEl.sendKeys('bbb');
+    page.main.createEl.click();
+
+    expect(page.main.features.count()).to.eventually.equal(7);
+  });
 
 });
